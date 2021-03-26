44e9440f6b7b5eeef41d398c1699b16dc1a68826
Tang142857 <dfsa00001@qq.com>
2021-03-26 23:33:40 +0800

    small changes

--- main.py
+++ main.py
@@ -8,4 +8,26 @@ Fiction-master main file.
 @date: 2021-03-13
 Copyright(c): DFSA Software Develop Center
 """
+import sys
 import process
+
+
+class ArgList(object):
+    """Argument list serve."""
+    def __init__(self, argv: list):
+        """Formatting the arg list to python object"""
+        print(f'Received arg list {argv}')
+        for index, element in enumerate(argv[1:]):  # ignore the first argument ,always file position.
+            if element.startswith('-'):
+                setattr(self, element[1:], argv[index + 2])
+
+    def __str__(self):
+        return str(dir(self))
+
+
+if __name__ == '__main__':
+    arg = ArgList(sys.argv)
+    print(f'Generating tasks argument list...')
+    argument={
+        'name':
+    }
